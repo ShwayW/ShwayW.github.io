1c4f7021a5cda63f1c88e97958b7257f3c0a3241
Ubuntu <ubuntu@ubuntu2010.linuxvmimages.local>
2021-02-08 17:27:41 -0700

    test

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://shwayw.github.io'
+SITEURL = 'https://shwayw.github.io/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
